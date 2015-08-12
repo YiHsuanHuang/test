f0e9d51f0355e3d510a61eb16c6781e00ed29e38
Yi-Hsuan, Huang <b02611026@ntu.edu.tw>
2015-08-12 10:45:16 +0800

    test2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,20 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QString str = "Hello";
+    qDebug() << str;
+    str.prepend("sdf");
+    qDebug() << str;
+    str.append("sss");
+    qDebug() << str;
+
+    QString str2 = "hry";
+    QString str3 = str2 + str;
+
+    str3.clear();
+
+    qDebug() << str.size();  //空白鍵也會算入
 }
 
 MainWindow::~MainWindow()
